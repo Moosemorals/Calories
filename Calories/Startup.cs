5d2b3214d0f4a4acba9a7a27471375d7ff79a811
Osric Wilkinson <gitmoose@fluffypeople.com>
2020-06-13 21:44:14 +0100

    Strip out compliation at runtime

--- Calories/Startup.cs
+++ Calories/Startup.cs
@@ -19,7 +19,7 @@ namespace Calories
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CalorieContext>();
 
@@ -40,11 +40,6 @@ namespace Calories
             services.AddTransient<AuthService>();
 
             services.AddRazorPages();
-
-            if (env.IsDevelopment())
-            {
-                services.AddRazorPages().AddRazorRuntimeCompilation();
-            }
 
             services.AddMvc();
         }
